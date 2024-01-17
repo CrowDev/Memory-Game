33b49db08283cb4b8342053a8ef2fede2e04dd76
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-17 00:43:59 -0300

    fetch and display images

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImagesService } from '../../services/images.service';
+import { CommonModule } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common'
+import { Entry, Result } from '../../@types';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
+  entries: Entry[] = [];
+
+  constructor(private imagesService: ImagesService) {}
+
+  ngOnInit(): void {
+    this.fetchImages();
+  }
+
+  fetchImages() {
+    this.imagesService.getImages().subscribe((data: Result) => {
+      console.log(data);
+      this.entries = data.entries;
+    }, (error) => console.log(error));
+  }
 }
