91ca228bc8d8ba5949a179c2c20534c2ba3050fc
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-17 02:34:33 -0300

    apply animation based on click event

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,14 +1,26 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Entry } from '../../@types';
+import { FacadeService } from '../../services/facade.service';
+import { animation } from '../../util/animation';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [NgOptimizedImage],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
+  animations: [animation.reveal]
 })
 export class CardComponent {
   @Input() entry!: Entry;
+  revealed = false;
+
+  constructor(private facadeService: FacadeService) {}
+
+  handleClick() {
+    this.revealed = true;
+    const uuid = this.entry.fields.image.uuid;
+    this.facadeService.handlePlay(uuid);
+  }
 }
