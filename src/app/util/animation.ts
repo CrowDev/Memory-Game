504180ba399c927af5bcc448e47e3fbc2a5456b1
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-18 02:11:19 -0300

    fix behaviour for a new game

--- src/app/util/animation.ts
+++ src/app/util/animation.ts
@@ -4,7 +4,7 @@ export const animation = {
   reveal: trigger('reveal', [
     state('not-revealed', style({ transform: 'rotateY(0deg)'})),
     state('revealed', style({ transform: 'rotateY(180deg)' })),
-    transition('void => *', animate('0.2s ease-in')),
-    transition('* => void', animate('0.2s ease-in')),
+    transition('void => *', animate('1s ease')),
+    transition('* => void', animate('1s ease')),
   ])
 }
