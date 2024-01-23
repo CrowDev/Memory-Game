df1f7f8a2eeb338b2676396db03b91d38357fac2
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-22 21:22:05 -0300

    add more tests for facade service

--- src/app/services/facade.service.spec.ts
+++ src/app/services/facade.service.spec.ts
@@ -1,18 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-
 import { FacadeService } from './facade.service';
 import { ImagesService } from './images.service';
 import * as expectedImages from '../util/mock-data.json';
 import { Entry, Result } from '../@types';
 
+
+// TODO: test observables, subjects and reset game
 describe('FacadeService', () => {
   let imageServiceSpy: jasmine.SpyObj<ImagesService>;
   let service: FacadeService;
   let entries: Entry[];
 
   beforeEach(() => {
-    // TestBed.configureTestingModule({});
-    // service = TestBed.inject(FacadeService);
     imageServiceSpy = jasmine.createSpyObj('ImageService', ['getImages']);
     service = new FacadeService(imageServiceSpy);
     const result: Result = expectedImages;
@@ -38,5 +36,124 @@ describe('FacadeService', () => {
     service.setMaxCorrects(10);
     service.corrects = Array(10).fill('');
     expect(service.isWin()).toBe(true);
+  });
+
+  it('should set current selected card', () => {
+    const card = { uuid: '1', index: 0 };
+    expect(service.currentSelectedCard)
+      .withContext('current card is null')
+      .toBe(null);
+    service.validateCurrentSelectedCard(card);
+    expect(service.currentSelectedCard)
+      .withContext('current card is set')
+      .toEqual(card);
+  });
+
+  it('should add 1 to count play', () => {
+    const card = { uuid: '1', index: 0 };
+    service.validatePlay(card);
+    expect(service.countPlay).toEqual(1);
+    expect(service.hit$.value).toEqual(0);
+    expect(service.missed$.value).toEqual(0);
+  });
+
+  it('should add 1 to hit', () => {
+    const card1 = { uuid: '1', index: 0 };
+    const card2 = { uuid: '1', index: 1 };
+    service.validateCurrentSelectedCard(card1);
+    service.countPlay = 1;
+    service.validatePlay(card2);
+    expect(service.hit$.value).toEqual(1);
+  });
+
+  it('should add 1 to wrong', () => {
+    const card1 = { uuid: '1', index: 0 };
+    const card2 = { uuid: '11', index: 1 };
+    service.validateCurrentSelectedCard(card1);
+    service.countPlay = 1;
+    service.validatePlay(card2);
+    expect(service.missed$.value).toEqual(1);
+  });
+
+  it('should doing nothing if click same card', () => {
+    const card = { uuid: '1', index: 0 };
+    service.validateCurrentSelectedCard(card);
+    service.countPlay = 1;
+    service.validatePlay(card);
+    expect(service.hit$.value).toEqual(0);
+    expect(service.missed$.value).toEqual(0);
+    expect(service.countPlay).toEqual(1);
+  });
+
+  it('should reset states after 2 cards clicked', () => {
+    const card1 = { uuid: '1', index: 0 };
+    const card2 = { uuid: '11', index: 1 };
+    expect(service.currentSelectedCard)
+      .withContext('there is no current play yet')
+      .toBe(null);
+    expect(service.countPlay)
+      .withContext('there is no current play yet')
+      .toBe(0);
+    service.validateCurrentSelectedCard(card1);
+    service.validatePlay(card1);
+    expect(service.currentSelectedCard)
+      .withContext('there is current play')
+      .toEqual(card1);
+    expect(service.countPlay)
+      .withContext('there is current play')
+      .toBe(1);
+    service.validatePlay(card2);
+    expect(service.currentSelectedCard)
+      .withContext('after 2 cards clicked, current play is reset')
+      .toBe(null);
+    expect(service.countPlay)
+      .withContext('after 2 cards clicked, current play is reset') 
+      .toBe(0);
+  });
+
+  it('should verify whetever user is stored or not', () => {
+    localStorage.removeItem('user');
+    expect(service.isUserStored())
+      .withContext('no user is stored')
+      .toBe(false);
+    service.storeUser('User');
+    expect(service.isUserStored())
+      .withContext('user is stored')
+      .toBe(true);
+  });
+
+  it('should store user', () => {
+    service.storeUser('User');
+    expect(localStorage.getItem('user')).toEqual('User');
+  });
+
+  it('should retrieve user', () => {
+    service.storeUser('User');
+    expect(service.getUser()).toEqual('User');
+  });
+
+  it('should check if clicking the exact same card', () => {
+    const card1 = { uuid: '1', index: 0 };
+    const card2 = { uuid: '1', index: 1 };
+    service.countPlay = 1;
+    service.validateCurrentSelectedCard(card1);
+    expect(service.isClickingExactSameCard(card1)).toBe(true);
+    expect(service.isClickingExactSameCard(card2)).toBe(false);
+  });
+
+  it('should check if clicking the a cloned card', () => {
+    const card1 = { uuid: '1', index: 0 };
+    const card2 = { uuid: '1', index: 1 };
+    service.validateCurrentSelectedCard(card1);
+    expect(service.isClickingClonedCard(card2)).toBe(true);
+    expect(service.isClickingClonedCard(card1)).toBe(false);
+  });
+
+  it('should check if user win', () => {
+    service.setMaxCorrects(10);
+    service.corrects = Array(10).fill('');
+    expect(service.isWin()).toBe(true);
+    service.corrects = Array(5).fill('');
+    expect(service.isWin()).toBe(false);
   });
 });
