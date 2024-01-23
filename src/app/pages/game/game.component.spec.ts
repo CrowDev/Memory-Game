f0fe8fdf3dbd945e9f1b22c612d9781daff74a3c
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-22 21:22:36 -0300

    wip test game component

--- src/app/pages/game/game.component.spec.ts
+++ src/app/pages/game/game.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
+import { FacadeService } from '../../services/facade.service';
 
 describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
+  let facadeServiceStub: Partial<FacadeService>;
+
+  facadeServiceStub = {
+    
+  }
 
   beforeEach(async () => {
+    const facadeService = jasmine.createSpyObj('FacadeService', ['initGame', 'isGameFinished', 'restartGame']);
     await TestBed.configureTestingModule({
-      imports: [GameComponent]
+      imports: [GameComponent],
+      providers: [
+        { provide: FacadeService, useValue: facadeService}
+      ]
     })
     .compileComponents();
     
