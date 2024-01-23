0f66dcdcc3167671b60c9ebccede077559d21334
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-23 02:40:19 -0300

    handle behaviour for difficult selector

--- src/app/components/difficult-selector/difficult-selector.component.ts
+++ src/app/components/difficult-selector/difficult-selector.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-difficult-selector',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './difficult-selector.component.html',
   styleUrl: './difficult-selector.component.css'
 })
 export class DifficultSelectorComponent {
+  showDialog = false;
+  closingAnimation = false;
+  difficulties = [
+    {label: 'easy (10 cards)', value: 'easy'},
+    {label: 'medium (20 cards)', value: 'medium'},
+    {label: 'hard (30 cards)', value: 'hard'},
+    {label: 'very hard (40 cards)', value: 'very-hard'},
+  ];
+  selectedDifficult = 'easy';
 
+  toggleDialog() {
+    if (!this.showDialog) {
+      this.closingAnimation = false;
+      this.showDialog = true;
+    } else {
+      this.closingAnimation = true;
+      setTimeout(() => {
+        this.showDialog = false;
+      }, 900);
+    }
+  }
+
+  onDifficultChange(difficulty: string) {
+    this.selectedDifficult = difficulty;
+    this.toggleDialog();
+  }
 }
