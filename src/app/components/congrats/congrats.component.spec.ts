f2820ee72c3ab72b20aefe10cddea518b2aca533
CrowDev <h.aguilarvalenzuela@gmail.com>
2024-01-23 00:03:20 -0300

    add base test congrats component

--- src/app/components/congrats/congrats.component.spec.ts
+++ src/app/components/congrats/congrats.component.spec.ts
@@ -1,20 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CongratsComponent } from './congrats.component';
+import { FacadeService } from '../../services/facade.service';
 
 describe('CongratsComponent', () => {
   let component: CongratsComponent;
   let fixture: ComponentFixture<CongratsComponent>;
+  let facadeService: jasmine.SpyObj<FacadeService>;
 
   beforeEach(async () => {
+    facadeService = jasmine.createSpyObj('FacadeService', ['getUser']);
     await TestBed.configureTestingModule({
-      imports: [CongratsComponent]
-    })
-    .compileComponents();
+      imports: [CongratsComponent],
+      providers: [
+        { provide: FacadeService, useValue: facadeService }
+      ]
+    });
     
     fixture = TestBed.createComponent(CongratsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
